18a3490d24e99ad69adf46a19de136617cb18d51
BenTaylor-TfN (Sneezy) <benjamin.taylor@transportforthenorth.com>
2023-04-11 11:47:50 +0100

    Rename template

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import versioneer
 
 if __name__ == "__main__":
     setup(
-        name="caf.{package_name}",
+        name="caf.distribute",
         version=versioneer.get_version(),
         cmdclass=versioneer.get_cmdclass(),
     )
