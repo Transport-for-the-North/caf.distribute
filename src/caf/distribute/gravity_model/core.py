f066d401d4c19a2fd6ace063107f73eb8c4fa44d
BenTaylor-TfN (Sneezy) <benjamin.taylor@transportforthenorth.com>
2023-04-13 09:21:06 +0100

    Integrate new toolkit functionality

--- src/caf/distribute/gravity_model/core.py
+++ src/caf/distribute/gravity_model/core.py
@@ -20,6 +20,8 @@ from caf.toolkit import pandas_utils as pd_utils
 # Local Imports
 # pylint: disable=import-error,wrong-import-position
 from caf.toolkit import timing
+from caf.toolkit import math_utils
+from caf.toolkit import cost_utils
 from caf.distribute import cost_functions
 # pylint: enable=import-error,wrong-import-position
 
