343cd279f2f32938238acb2f2bc1746388a5cdf7
Isaac Scott <isaac.scott@transportforthenorth.com>
2025-09-05 10:27:17 +0100

    Hopefully fixes tests, passing locally now

--- src/caf/distribute/array_utils.py
+++ src/caf/distribute/array_utils.py
@@ -246,11 +246,11 @@ def _flatten_some_sparse_axis_with_transpose(
     # Transpose and get the flat coordinates out
     array = array.transpose(axis_swap)
     get_vals = list(range(len(flatten_axis)))
-    flat_coord = np.ravel_multi_index(
+    flat_coord = np.array(np.ravel_multi_index(
         tuple(np.take(array.coords, get_vals, axis=0)),
         tuple(np.take(array.shape, get_vals)),
-    )
-    flat_coord[0].sort()
+    ))
+    flat_coord.sort()
     return flat_coord, array, axis_swap_reverse
 
 
@@ -262,12 +262,12 @@ def _flatten_some_sparse_axis_without_transpose(
 
     Will only work when flat axis are sequential and in order
     """
-    flat_coord = np.ravel_multi_index(
+    flat_coord = np.array(np.ravel_multi_index(
         tuple(np.take(array.coords, flatten_axis, axis=0)),
         tuple(np.take(array.shape, flatten_axis)),
-    )
+    ))
 
-    flat_coord[0].sort()
+    flat_coord.sort()
     return flat_coord
 
 
